6c2aecfd4572a0c4e173b18a9a3ddc1e0c9f5100
Alena Nikulina <alenanikulina0@gmail.com>
2019-12-04 16:56:01 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,9 +1,11 @@
-let state = {count: 0};
+let state;  // declare the state first, instead of assigning it to equal to smth
 
-function changeState(state, action){
+function changeState(state = { count: 0 }, action){  // passing a default argument of state to our educer function
     switch (action.type) {
+
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
+
       default:
         return state;
     }
@@ -17,3 +19,6 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+
+dispatch({ type: '@@INIT' })
+// dispatch({ type: 'INCREASE_COUNT' })
